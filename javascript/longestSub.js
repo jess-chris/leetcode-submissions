5c7a7d5aab6b17d92f1d802fd285c7dfbe719337
Jesse Christensen <jtq.christensen@gmail.com>
2022-05-09 11:55:41 -0700

    longest unique substring

---
+++ javascript/longestSub.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(s) {
+    
+  let longest = s.length > 1 ? '' : s[0];
+
+  let substr = s.length > 1 ? '' : s[0];
+
+  for (let x = 0; x < s.length; x++) {
+
+    if (!substr.includes(s[x])) {
+      substr += s[x];
+      if (substr.length >= longest.length) longest = substr;
+      console.log(`current - ${substr}`)
+    } else {
+      substr = substr.slice(substr.indexOf(s[x])+1, x)
+      if (substr !== s[x]) substr += s[x];
+      if (substr.length >= longest.length) longest = substr;
+    }
+  }
+
+  return longest?.length || 0;
+
+};
+
+
+const test = "anviaj";
+
+console.log(lengthOfLongestSubstring(test));
